44177ddac0c96e148476c51735ecf6878dfa27af
Gleb Garunchik <garunchikgleb@gmail.com>
2020-05-26 22:27:32 +0300

    added test listener

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,14 +2,14 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import pages.*;
 import utils.CapabilitiesGenerator;
+import utils.TestListener;
 
-import java.util.concurrent.TimeUnit;
-
+@Listeners(TestListener.class)
 public class BaseTest {
 
     private WebDriver driver;
