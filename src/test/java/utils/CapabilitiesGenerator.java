bac247cf355e87cbdf9bfa6a4a8731217c23366a
Gleb Garunchik <garunchikgleb@gmail.com>
2020-05-27 21:42:45 +0300

    Update CapabilitiesGenerator.java

--- src/test/java/utils/CapabilitiesGenerator.java
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -25,7 +25,7 @@ public class CapabilitiesGenerator {
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-notifications");
 
-        //options.addArguments("--headless"); // only if you are ACTUALLY running headless
+        options.addArguments("--headless"); // only if you are ACTUALLY running headless
         //options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
         //options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
         //options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
